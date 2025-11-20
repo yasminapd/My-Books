665c5481b1865e061d02fb899b14f88b8b572796
yasminapd <167645177+yasminapd@users.noreply.github.com>
2025-11-20 16:13:03 +0000

    Add searchbar to my-books page

--- fronted/src/app/my-books/my-books.page.ts
+++ fronted/src/app/my-books/my-books.page.ts
@@ -2,32 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BookService } from '../services/book-service';
 
-
 @Component({
   selector: 'app-my-books',
   templateUrl: './my-books.page.html',
   styleUrls: ['./my-books.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class MyBooksPage implements OnInit {
+  books: any[] = [];
+  filteredBooks: any[] = [];
 
-  books: any = [];
-
-  constructor(private bookService: BookService, private router: Router) { }
+  constructor(private bookService: BookService, private router: Router) {}
 
   ngOnInit() {
     this.getAllBooks();
   }
 
   ionViewWillEnter() {
-    // Se ejecuta cada vez que se entra a esta página
     this.getAllBooks();
   }
 
   getAllBooks() {
-    this.bookService.getBooks().subscribe(response => {
-      this.books = response;
+    this.bookService.getBooks().subscribe((response) => {
+      const books = Array.isArray(response) ? response : [];
+      this.books = books;
+      this.filteredBooks = [...books];
     });
+  }
+
+  filterBooks(event: Event) {
+    const value = (event as CustomEvent).detail.value?.toLowerCase() || '';
+    this.filteredBooks = this.books.filter((book) =>
+      book.title?.toLowerCase().includes(value)
+    );
   }
 
   editBook(id: number) {
@@ -37,7 +44,7 @@ export class MyBooksPage implements OnInit {
   deleteBook(id: number) {
     if (confirm('¿Estás seguro de eliminar este libro?')) {
       this.bookService.deleteBook(id).subscribe(() => {
-        this.getAllBooks(); // Recargar lista
+        this.getAllBooks();
       });
     }
   }
